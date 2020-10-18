659f22e72c04040a656c56aff2f5cede4418d2fb
Tom Smalley <tfsmalley@gmail.com>
2020-10-18 17:59:01 +0100

    Add linear fog calculation

--- src/VertexShaders/fog/RLDoubleTexture.hlsl
+++ src/VertexShaders/fog/RLDoubleTexture.hlsl
@@ -17,8 +17,13 @@ struct PS_OUTPUT
 };
 
 uniform float4x4 view_matrix : register(c0);
-uniform float4x4 unknown_c20 : register(c20);
+uniform float4x4 look_at : register(c20);
 uniform float4 light_direction : register(c41);
+// x = fog start
+// y = fog end
+// z = 1 / (fog end - fog start)
+// w = skybox saturation?
+uniform float4 fog_settings : register(c45);
 uniform float4 view_pos : register(c50);
 uniform float4 unknown_c55 : register(c55);
 uniform float4 unknown_c56 : register(c56);
@@ -27,24 +32,41 @@ uniform float4 unknown_c60 : register(c60);
 uniform float4 unknown_c61 : register(c61);
 uniform float4 unknown_c62 : register(c62);
 uniform float4 unknown_c63 : register(c63);
+// x = some fog constant
 uniform float4 unknown_c64 : register(c64);
 uniform float4 unknown_c65 : register(c65);
 uniform float4 unknown_c66 : register(c66);
 
 void main(in PS_INPUT input, out PS_OUTPUT output)
 {
+    // line 22
     output.position = mul(input.position, view_matrix);
-    output.fog = 0;
 
+    // --- Linear Fog
+    // line 14
+    float4 r9 = mul(input.position, look_at);
+
+    // line 18
+    float r9x = fog_settings.y - r9.z;
+    r9.z = r9x * fog_settings.z;
+
+    // line 22
+    r9.z = min(r9.z, unknown_c64.x);
+
+    // line 28
+    output.fog = r9.z;
+
+    // --- Scattering
+    // line 11
     //float4 view_vector = normalize(view_pos - input.position);
     //float r0x = dot(view_vector, light_direction);
     //r0x = min(r0x, unknown_c64.w);
     //float r0y = r0x * r0x + unknown_c64.x;
 
     // Line 24
-    float4 r1 = mul(input.position, unknown_c20);
+    float4 r1 = mul(input.position, look_at);
     float r0z = mul(r1.z, unknown_c66.x);
-    
+
     // Line 33
     r1 = mul(unknown_c61, (- r0z));
     r1 = mul(r1, unknown_c64.y);
@@ -59,7 +81,7 @@ void main(in PS_INPUT input, out PS_OUTPUT output)
     // Line 75+2
     r3 = r3 * unknown_c55;
     r3 = mul(r3, unknown_c55.w);
-    
+
     output.vcol.xyz = input.color.xyz * r3.xyz;
     output.vcol.w = input.color.w;
 
